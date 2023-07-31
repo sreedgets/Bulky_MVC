ddd6efa8277bd071aa903739b01d7a01f1a5aa56
Skyler <sreedgets@outlook.com>
2023-07-31 16:05:28 -0500

    Add project files.

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        //Creating a property so that EF Core can interact with the specified
+        //table in the DB. DbSet<ClassNameOfModel> NameOfSqlTable
+        public DbSet<Category> Categories { get; set; }
+    }
+}
