c8d25db12f2fb607cfcb8e087c4d33c73c11c25a
Skyler <sreedgets@outlook.com>
2023-07-31 17:48:17 -0500

    Seed Category table in DB

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -13,5 +13,17 @@ namespace BulkyWeb.Data
         //Creating a property so that EF Core can interact with the specified
         //table in the DB. DbSet<ClassNameOfModel> NameOfSqlTable
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Category[] categories = new Category[]
+            {
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Fantasy", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+            };
+
+            modelBuilder.Entity<Category>().HasData(categories);
+        }
     }
 }
