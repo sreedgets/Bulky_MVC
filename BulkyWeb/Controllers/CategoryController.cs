28fb1a4201d19213c9733e2ee5e40e879e16427d
Skyler <sreedgets@outlook.com>
2023-07-31 20:38:23 -0500

    Build category list view

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Category> categories = _db.Categories.ToList();
+            //var categories = _db.Categories;
+
+            return View(categories);
         }
     }
 }
